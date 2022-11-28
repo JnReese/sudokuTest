bf832f8e2fada6b7fcce46727a4078ff4e6e2124
JnReese <jonreese777@gmail.com>
2022-11-28 16:53:25 -0500

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  //@ts-ignore
-  baseUrl: "http://localHost3000",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localHost3000",
   },
 });
